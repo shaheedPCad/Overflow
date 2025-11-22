9adfc177806d699bd26c4cbe1be267948c167cf2
shaheed <shaheedPCad@gmail.com>
2025-11-22 12:01:32 -0500

    Implemented CRUD operations

---
+++ QuestionService/DTOs/CreateQuestionDto.cs
@@ -0,0 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+using QuestionService.Validators;
+
+namespace QuestionService.DTOs;
+
+public record CreateQuestionDto([Required]string Title, [Required]string Content, 
+    [Required][TagListValidator(1, 5)]List<string> Tags);
